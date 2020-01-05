98f87aa9b0d7c251d9e944c4cd3850aacc8002d9
Khena <khena.jacobs@outlook.com>
2020-01-05 15:09:21 +0100

    Add express

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const token = "NjYxMzM2OTI1MzMxNzgzNzAx.XhG9iQ.qeLPlA1OnO-Ye2_js0azgVfpc_A"
 const PREFIX = 'patron ';
 var song_queue = {};
 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 bot.on('ready', () =>{
         console.log("Cartel of Medellin is ready!")
