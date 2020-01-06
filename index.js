4057a2493a7039defa0952a008a9d3d4e72fc610
Khena <khena.jacobs@outlook.com>
2020-01-06 18:33:14 +0100

    change http

--- index.js
+++ index.js
@@ -13,16 +13,20 @@ var song_queue = {};
 var express = require('express');
 var app     = express();
 
-http.createServer(onRequest).listen(process.env.PORT || 6000)
+// http.createServer(onRequest).listen(process.env.PORT || 6000)
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+// //For avoidong Heroku $PORT error
+// app.get('/', function(request, response) {
+//     var result = 'App is running'
+//     response.send(result);
+// }).listen(app.get('port'), function() {
+//     console.log('App is running, server is listening on port ', app.get('port'));
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
-
 
 bot.on('ready', () =>{
         console.log("Cartel of Medellin is ready!")
