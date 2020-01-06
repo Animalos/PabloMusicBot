75536b96d7fadd308f7caf401bec5ba522555d27
Khena <khena.jacobs@outlook.com>
2020-01-06 18:51:49 +0100

    Final update

--- index.js
+++ index.js
@@ -13,16 +13,6 @@ var song_queue = {};
 var express = require('express');
 var app     = express();
 
-// http.createServer(onRequest).listen(process.env.PORT || 6000)
-
-// //For avoidong Heroku $PORT error
-// app.get('/', function(request, response) {
-//     var result = 'App is running'
-//     response.send(result);
-// }).listen(app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
@@ -32,7 +22,6 @@ bot.on('ready', () =>{
         console.log("Cartel of Medellin is ready!")
         bot.user.setStatus('online')
         bot.user.setActivity('Selling Narcos, puta!')
-        // bot.user.setActivity("Samme's kleine piemel")
 })
 
 bot.on('message',message =>{
@@ -43,7 +32,6 @@ bot.on('message',message =>{
                 var song = song_queue[message.guild.id];
                 
                 song.queue.push(args[1]);
-
 
             break;
 
